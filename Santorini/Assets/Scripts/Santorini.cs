4b3fade5f85d5d3ef00e4ac98d288e7dc0460237
DaynaLacoursiere <daynalacoursiere@gmail.com>
2019-06-25 20:56:58 -0600

    Added tiles and Santorini code file

---
+++ Santorini/Assets/Scripts/Santorini.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Santorini : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        try
+        {
+
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log(e);
+            Debug.Break();
+        }
+    }
+}
