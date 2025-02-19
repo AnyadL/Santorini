d1144818edc64a43e2e105fa90233325a5da8d77
AnyadL <daynalacoursiere@gmail.com>
2025-02-19 15:56:14 -0700

    Added the Minotaur - His workers may move into an opponents space if the opponent worker can be forced straight backwards to an unoccupied space at any level. Still have a bug where the opponent worker wins if pushed onto level 3

--- Santorini/Assets/Scripts/Player.cs
+++ Santorini/Assets/Scripts/Player.cs
@@ -34,7 +34,7 @@ public class Player
 
         if(colour == Worker.Colour.Blue)
         {
-            _god = new Apollo();
+            _god = new Minotaur();
         } else {
             _god = new Artemis();
         }
